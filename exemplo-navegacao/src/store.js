70cafd30ccce1b12936b8a8e17b0ff9bec69c72b
Márcio Bueno <marciobueno@gmail.com>
2021-10-25 18:52:27 -0300

    Fix easy-peasy store

--- exemplo-navegacao/src/store.js
+++ exemplo-navegacao/src/store.js
@@ -1,12 +1,18 @@
 import { action, createStore } from "easy-peasy";
 
 import { themes, isSameTheme } from "./themes";
+import { composeWithDevTools } from "remote-redux-devtools";
 
-export const store = createStore({
-  theme: themes.dark,
-  toggleTheme: action((state) => {
-    state.theme = isSameTheme(state.theme, themes.dark)
-      ? themes.light
-      : themes.dark;
-  }),
-});
+export const store = createStore(
+  {
+    theme: themes.dark,
+    toggleTheme: action((state) => {
+      state.theme = isSameTheme(state.theme, themes.dark)
+        ? themes.light
+        : themes.dark;
+    }),
+  },
+  {
+    compose: composeWithDevTools({ realtime: true, trace: true }),
+  }
+);
